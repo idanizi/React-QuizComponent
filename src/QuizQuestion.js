d83cc6c8603ed4c032acec45a84b2e9be4961708
idanizi <idanizi@gmail.com>
2019-03-25 18:16:02 +0200

    module 2

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -1,17 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export default class QuizQuestion extends React.Component {
+class QuizQuestion extends Component {
 
-    render = () =>
-        <main>
-            <section>
-                <p>{this.props.quiz_question.instruction_text}</p>
-            </section>
-            <section className="buttons">
-                <ul>
-                    <li>{this.props.quiz_question.answer_options[0]}</li>
-                </ul>
-            </section>
-        </main>
+    render() {
+        return (
+            <main>
+                <section>
+                    <p>{this.props.quiz_question.instruction_text}</p>
+                </section>
+                <section className="buttons">
+                    <ul>
+                        <li>{this.props.quiz_question.answer_options[0]}</li>
+                    </ul>
+                </section>
+            </main>
+        );
+    }
 
 }
+
+export default QuizQuestion;
